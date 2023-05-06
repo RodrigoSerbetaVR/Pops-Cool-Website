e4a703599c8b28ed6cb912794b7690d532d000ba
RodrigoSerbetaVR <120190032+RodrigoSerbetaVR@users.noreply.github.com>
2023-05-06 06:48:53 +0300

    Update LoginUsers.js

--- LoginUsers.js
+++ LoginUsers.js
@@ -1,19 +1,20 @@
 const users = [
-    { username: "RodrigoSerbeta", password: "ImCoolGus" }
-  ];
-  
-  const form = document.querySelector("form");
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    const username = form.elements.username.value;
-    const password = form.elements.password.value;
-    const user = users.find(user => user.username === username && user.password === password);
-    if (user) {
-      alert("Login successful!");
-      window.location.href = "PopsWebsite.html";
-      form.reset();
-    } else {
-      alert("Invalid username or password!");
-    }
-  });
-  
+  { username: "RodrigoSerbeta", password: "ImCoolGus" },
+  { username: "John", password: "Hailhitler69" },
+  { username: "test1", password: "test2" }
+];
+
+const form = document.querySelector("form");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const username = form.elements.username.value;
+  const password = form.elements.password.value;
+  const user = users.find(user => user.username === username && user.password === password);
+  if (user) {
+    alert("Login successful!");
+    window.location.href = "PopsWebsite.html";
+    form.reset();
+  } else {
+    alert("Invalid username or password!");
+  }
+});
